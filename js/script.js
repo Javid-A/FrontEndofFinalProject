a2deed0ce1823f18b0c07ada5bb95d6e44c8d342
Javid <javidia@code.edu.az>
2020-08-03 17:14:31 +0400

    Search End

--- js/script.js
+++ js/script.js
@@ -66,6 +66,64 @@ $(document).ready(function() {
         $(".activeMenu").fadeOut()
     })
 
+    // Search
+    $("#searchBtn").click(function() {
+        $(".search-box").css("transition", "0.6s ease-in")
+        $(".search-box").css("border-radius", "40px")
+        $(".search-box").css("background", "turquoise")
+        $("#searchBtn").css("opacity", "0")
+
+        setTimeout(function Width() {
+            $(".search-box").css("width", "320px")
+        }, 600)
+        setTimeout(function CloseBtn() {
+            $("#closeBtn").css("display", "block")
+        }, 600)
+        setTimeout(function CloseBtn() {
+            $("#closeBtn").css("opacity", "1")
+        }, 1200)
+        setTimeout(function SearchBtn() {
+            $("#searchBtn").css("display", "none")
+        }, 600)
+        setTimeout(function Form() {
+            $("#Search #form").css("display", "flex")
+        }, 600)
+        setTimeout(function Form() {
+            $("#Search #form").css("opacity", "1")
+        }, 1200)
+        $(".search-box").css("transform", "rotateZ(360deg)")
+    })
+    $("#closeBtn").click(function() {
+        // $(".search-box").css("transition", "0.6s ease-in")
+        // $(".search-box").css("border-radius", "50px")
+        // $(".search-box").css("background", "#236bdf")
+        $("#closeBtn").css("opacity", "0")
+        $("#Search #form").css("opacity", "0")
+            // $("#searchBtn").css("opacity", "1")
+
+
+        setTimeout(function Width() {
+            $(".search-box").css("width", "50px")
+        }, 100)
+        setTimeout(function CloseBtn() {
+            $("#closeBtn").css("display", "none")
+        }, 600)
+        setTimeout(function SearchBtn() {
+            $("#searchBtn").css("display", "block")
+        }, 600)
+        setTimeout(function SearchBtn() {
+            $("#searchBtn").css("opacity", "1")
+        }, 700)
+        setTimeout(function Form() {
+            $("#Search #form").css("display", "none")
+        }, 800)
+        setTimeout(function SearchBtn() {
+            $(".search-box").css("transform", "rotateZ(-360deg)")
+            $(".search-box").css("border-radius", "0px")
+            $(".search-box").css("background", "#236bdf")
+        }, 800)
+    })
+
     // Slider start
     var swiper = new Swiper('.swiper-container', {
         effect: 'coverflow',
